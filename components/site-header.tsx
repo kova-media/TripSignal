a69eddd5da5f5134eacba624118eca714b1b35a5
kova-media <damian@kovamediagroup.com>
2026-09-05 18:53:56 -0500

    Connect header sign-in to authentication

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -16,7 +16,7 @@ export default function SiteHeader({ backHref, backLabel, primaryHref, primaryLa
       </a>
       <div className="nav-actions">
         {backHref && backLabel ? <a className="text-link" href={backHref}>{backLabel}</a> : null}
-        {!backHref && <a className="text-button" href="/alerts">Sign in</a>}
+        {!backHref && <a className="text-button" href="/signin">Sign in</a>}
         <ThemeToggle />
         {primaryHref && primaryLabel ? <a className="button button-primary" href={primaryHref}>{primaryLabel}</a> : !backHref ? <a className="button button-primary" href="/alerts">Create alert</a> : null}
       </div>
