cacbc099d5cd4763e78c0c1b9d03b89876e76c1c
kova-media <damian@kovamediagroup.com>
2026-09-09 10:10:57 -0500

    Fix web app manifest icon purpose

--- app/manifest.ts
+++ app/manifest.ts
@@ -14,7 +14,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/tripsignal-icon-blue.svg',
         sizes: 'any',
         type: 'image/svg+xml',
-        purpose: 'any maskable',
+        purpose: 'any',
       },
     ],
   };
