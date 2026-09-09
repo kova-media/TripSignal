363ed59e012ea478159995d02358bcb449fa07d0
kova-media <damian@kovamediagroup.com>
2026-09-09 09:16:59 -0500

    Restore Stripe billing client

---
+++ lib/stripe.ts
@@ -0,0 +1,16 @@
+import Stripe from 'stripe';
+
+let stripeClient: Stripe | null = null;
+
+export function getStripe() {
+  const secretKey = process.env.STRIPE_SECRET_KEY;
+  if (!secretKey) throw new Error('STRIPE_SECRET_KEY is not configured.');
+  if (!stripeClient) stripeClient = new Stripe(secretKey);
+  return stripeClient;
+}
+
+export function getStripePriceId() {
+  const priceId = process.env.STRIPE_PRICE_ID;
+  if (!priceId) throw new Error('STRIPE_PRICE_ID is not configured.');
+  return priceId;
+}
