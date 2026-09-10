423097010386d864d7e40df66f780906c1bfbf33
kova-media <damian@kovamediagroup.com>
2026-09-10 10:59:02 -0500

    Fix Travelpayouts Drive script installation

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,16 +51,34 @@ const frequencyScript = `(() => {
   observer.observe(document.body, { childList: true, subtree: true });
 })();`;
 
-const travelpayoutsScript = `<script nowprocket data-noptimize="1" data-cfasync="false" data-wpfc-render="false" seraph-accel-crit="1" data-no-defer="1" data-cmp-ab="2">
-  (function () {
-      var script = document.createElement("script");
-      script.async = 1;
-      script.setAttribute("data-cmp-ab","2");
-      script.src = 'https://tp-em.com/NTcyNTI3.js?t=572527';
-      document.head.appendChild(script);
-  })();
-</script>`;
+const travelpayoutsScript = `(function () {
+  var script = document.createElement("script");
+  script.async = 1;
+  script.setAttribute("data-cmp-ab", "2");
+  script.src = 'https://tp-em.com/NTcyNTI3.js?t=572527';
+  document.head.appendChild(script);
+})();`;
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <html lang="en" suppressHydrationWarning><body><script dangerouslySetInnerHTML={{ __html: themeScript }} /><script dangerouslySetInnerHTML={{ __html: frequencyScript }} />{children}<script dangerouslySetInnerHTML={{ __html: travelpayoutsScript }} /></body></html>;
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          nowprocket="true"
+          data-noptimize="1"
+          data-cfasync="false"
+          data-wpfc-render="false"
+          seraph-accel-crit="1"
+          data-no-defer="1"
+          data-cmp-ab="2"
+          dangerouslySetInnerHTML={{ __html: travelpayoutsScript }}
+        />
+      </head>
+      <body>
+        <script dangerouslySetInnerHTML={{ __html: themeScript }} />
+        <script dangerouslySetInnerHTML={{ __html: frequencyScript }} />
+        {children}
+      </body>
+    </html>
+  );
 }
