c12b07b156f3059998b41c4182265e852be9823b
kova-media <damian@kovamediagroup.com>
2026-09-16 08:55:57 -0500

    Add crawler rules for search and AI discovery

---
+++ app/robots.ts
@@ -0,0 +1,21 @@
+import type { MetadataRoute } from 'next';
+
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL
+  ? process.env.NEXT_PUBLIC_SITE_URL
+  : process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : undefined;
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/account/', '/admin/', '/api/', '/profile/', '/signin/'],
+      },
+    ],
+    sitemap: siteUrl ? `${siteUrl.replace(/\/$/, '')}/sitemap.xml` : undefined,
+    host: siteUrl,
+  };
+}
